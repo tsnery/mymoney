4a51febe1e05150bad0504d146ca3519f313a464
Tainan Nery <tainan.nery@algoritme.com.br>
2022-02-10 02:13:42 -0300

    feat: added intl to format currency

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components'
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { LogBox } from 'react-native';
